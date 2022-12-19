f73e3b9f5ec67b9311ef5259b9e808016f8cf9c9
pill27211 <120912574+pill27211@users.noreply.github.com>
2022-12-19 19:11:25 +0900

    Update 14003_가장 긴 증가하는 부분 수열 5.cpp

--- Platinum/LIS/14003_가장 긴 증가하는 부분 수열 5/14003_가장 긴 증가하는 부분 수열 5.cpp
+++ Platinum/LIS/14003_가장 긴 증가하는 부분 수열 5/14003_가장 긴 증가하는 부분 수열 5.cpp
@@ -10,13 +10,10 @@ int main()
 	ios_base::sync_with_stdio(0); cin.tie(0);
 	cin >> n;
 	for (i = 1; i <= n; cin >> a[i++]);
-	V.push_back(a[1]);
-	for (i = 2; i <= n; i++)
+	for (V.push_back(a[1]), i = 2; i <= n; i++)
 	{
-		if (V[p] < a[i])
-			V.push_back(a[i]), dp[i] = ++p;
-		else
-			j = lower_bound(V.begin(), V.end(), a[i]) - V.begin(), V[j] = a[i], dp[i] = j;
+		j = lower_bound(V.begin(), V.end(), a[i]) - V.begin();
+		j == V.size() ? V.push_back(a[i]), dp[i] = ++p : (V[j] = a[i], dp[i] = j);
 	}
 	cout << p + 1 << '\n';
 	for (i = n; i; i--)
