85ae2fc911189c152567524b4ab17506bb2368da
pill27211 <120912574+pill27211@users.noreply.github.com>
2022-12-22 01:05:21 +0900

    Update 15949_Piet.cpp

--- Platinum/Implementation/15949_Piet/15949_Piet.cpp
+++ Platinum/Implementation/15949_Piet/15949_Piet.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int dy[] = { 0, -1, 0, 1, 0 }, dx[] = { 0, 0, 1, 0, -1 };
 vector <P> V1, V2;
 char M[105][105];
-int dp, cc, p, q, c;
+int dp, cc, p, q, f, c;
 int n, m, i, j;
 string an;
 
@@ -30,7 +30,7 @@ void find1()
 
 	while (Q.size())
 	{
-		auto[a, b] = Q.front(); Q.pop();
+		auto [a, b] = Q.front(); Q.pop();
 
 		for (i = 1; i <= 4; i++)
 		{
