224476ed33ae14eb8a6acdaf118358c6cdc569b3
pill27211 <120912574+pill27211@users.noreply.github.com>
2022-12-24 21:55:59 +0900

    Update 15481_그래프와 MST.cpp

--- Platinum/Graphs/15481_그래프와 MST/15481_그래프와 MST.cpp
+++ Platinum/Graphs/15481_그래프와 MST/15481_그래프와 MST.cpp
@@ -1,77 +1,110 @@
 #include<bits/stdc++.h>
 #define ll long long
-#define P pair<ll, ll>
+#define N 200001
 using namespace std;
 
-vector <P> Vx;
-vector <P> Vy;
-vector <P> V[400001];
-ll D[400001];
-ll N, K, i, j;
-ll x, y, z, p, q;
-
-void f1()
+typedef struct _Edge
 {
-	ios_base::sync_with_stdio(false); cin.tie(nullptr);
-	fill(D, D + 400001, 1e18);
-	cin >> N >> K;
+	int V1;
+	int V2;
+	ll W;
 
-	for (i = 1; i <= N; i++)
-	{
-		cin >> x >> y >> z;
+	bool operator<(struct _Edge& e) { return W < e.W; }
+}Edge;
 
-		Vx.push_back({ x, i });
-		Vy.push_back({ y, i });
+vector <Edge> Eg(N), CV(N);
+vector <pair<int, ll>> Gr[N];
+int P[N];
+int D[N], dp[N][19];
+int Mdp[N][19];
+int n, m, i, j;
+ll w, s;
 
-		p = z % K;
-		q = (K - p) % K;
-		V[i].push_back({ N + 1 + p, z });
-		V[N + 1 + q].push_back({ i, z });
-	}
+void f0()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> n >> m;
+	iota(P, P + N, 0);
+	for (i = 0; i < m; i++)
+		cin >> Eg[i].V1 >> Eg[i].V2 >> Eg[i].W;
 }
-void f2()
+int f1(int p)
 {
-	priority_queue <P> Q;
-	Q.push({ 0, 1 });
-	D[1] = 0;
-
-	while (!Q.empty())
-	{
-		i = -Q.top().first;
-		j = Q.top().second;
-		Q.pop();
-
-		if (D[j] < i)
-			continue;
-		for (int k = 0; k < (int)V[j].size(); k++)
-		{
-			ll d = i + V[j][k].second;
-			if (d < D[V[j][k].first])
-			{
-				D[V[j][k].first] = d;
-				Q.push({ -d, V[j][k].first });
-			}
-		}
-	}
+	return P[p] = P[p] == p ? p : f1(P[p]);
 }
 void f3()
 {
-	sort(Vx.begin(), Vx.end());
-	sort(Vy.begin(), Vy.end());
+	CV = Eg;
+	sort(CV.begin(), CV.begin() + m);
 
-	for (i = 1; i < N; i++)
+	for (i = 0; i < m; i++)
 	{
-		V[Vx[i].second].push_back({ Vx[i - 1].second, abs(Vx[i].first - Vx[i - 1].first) });
-		V[Vx[i - 1].second].push_back({ Vx[i].second, abs(Vx[i].first - Vx[i - 1].first) });
-		V[Vy[i].second].push_back({ Vy[i - 1].second, abs(Vy[i].first - Vy[i - 1].first) });
-		V[Vy[i - 1].second].push_back({ Vy[i].second, abs(Vy[i].first - Vy[i - 1].first) });
+		int o1 = f1(CV[i].V1);
+		int o2 = f1(CV[i].V2);
+
+		if (o1 ^ o2)
+		{
+			s += CV[i].W;
+			o1 > o2 ? P[o1] = o2 : P[o2] = o1;
+			Gr[CV[i].V1].push_back({ CV[i].V2, CV[i].W });
+			Gr[CV[i].V2].push_back({ CV[i].V1, CV[i].W });
+		}
 	}
-	f2();
 }
-int main(void)
+void f4(int p, int q)
 {
-	f1();
+	D[p] = q;
+	for (auto& a : Gr[p])
+		if (!D[a.first])
+			dp[a.first][0] = p, Mdp[a.first][0] = a.second, f4(a.first, q + 1);
+}
+void f5()
+{
+	for (j = 1; j < 19; j++)
+		for (i = 1; i <= n; i++)
+		{
+			dp[i][j] = dp[dp[i][j - 1]][j - 1];
+			Mdp[i][j] = max(Mdp[i][j - 1], Mdp[dp[i][j - 1]][j - 1]);
+		}
+}
+int f6(int p, int q)
+{
+	if (D[p] < D[q])
+		swap(p, q);
+	int g = 0, d = D[p] - D[q];
+
+	for (int k = 0; d; d >>= 1, k++)
+		if (d & 1)
+			g = max(g, Mdp[p][k]), p = dp[p][k];
+	if (p == q)
+		return g;
+	for (int k = 18; k >= 0; k--)
+		if (dp[p][k] != dp[q][k])
+		{
+			g = max(g, max(Mdp[p][k], Mdp[q][k]));
+			p = dp[p][k];
+			q = dp[q][k];
+		}
+	g = max(g, max(Mdp[p][0], Mdp[q][0]));
+	return g;
+}
+void f7()
+{
+	for (n = 0; n < m; n++)
+	{
+		i = Eg[n].V1;
+		j = Eg[n].V2;
+		w = Eg[n].W;
+
+		cout << s - f6(i, j) + w << '\n';
+	}
+}
+int main()
+{
+	f0();
 	f3();
-	for (i = 1; i <= N; i++)
-		cout << D[i] << '\n';
+	f4(1, 1);
+	f5();
+	f7();
 }
