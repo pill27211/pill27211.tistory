92e2b0b737830ae890968f08b37211cd99dc86eb
pill27211 <120912574+pill27211@users.noreply.github.com>
2022-12-25 22:45:58 +0900

    Add files via upload

---
+++ Gold/DP/16400_소수 화폐/16400_소수 화폐.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int n, i, p[40001], dp[40001];
+int main()
+{
+	for (i = 2; i <= 40000; i++)
+		if (!p[i])
+		{
+			for (n = i + i; n <= 40000; n += i)
+				p[n] = 1;
+			for (++dp[i], n = i; n <= 40000; n++)
+				dp[n] = (dp[n] + dp[n - i]) % 123456789;
+		}
+	std::cin >> n;
+	std::cout << dp[n];
+}
