0baa82eecc9b40266273f087ce6a9e1c51768649
pill27211 <120912574+pill27211@users.noreply.github.com>
2022-12-29 23:58:10 +0900

    Add files via upload

---
+++ Gold/DP/2698_인접한 비트의 개수/2698_인접한 비트의 개수.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long n, i, j, dp[101][101][2];
+int main()
+{
+	dp[2][0][0] = 2, dp[2][0][1] = 1, dp[2][1][1] = 1;
+	for (i = 3; i < 101; i++)
+		for (j = 0; j < i; j++)
+		{
+			dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
+			dp[i][j][1] = dp[i - 1][j][0] + (j ? dp[i - 1][j - 1][1] : 0);
+		}
+	std::cin >> n;
+	while (n--)
+		std::cin >> i >> j, std::cout << dp[i][j][0] + dp[i][j][1] << '\n';
+}
