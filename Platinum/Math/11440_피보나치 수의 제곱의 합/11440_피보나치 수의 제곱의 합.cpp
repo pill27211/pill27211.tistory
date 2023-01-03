9ae52d847b52c08b0097941fdedcea410ba7d197
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-01-04 00:31:20 +0900

    Create 11440_피보나치 수의 제곱의 합.cpp

---
+++ Platinum/Math/11440_피보나치 수의 제곱의 합/11440_피보나치 수의 제곱의 합.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+map <ll, ll> M;
+ll a, m = (ll)1e9 + 7;
+
+ll f(ll n)
+{
+    if (!M.count(n))
+    {
+        ll p = f(n / 2 + (n & 1 ? 1 : -1)), q = f(n >> 1);
+        M[n] = (n & 1 ? (p * p + q * q) : ((2 * p + q) * q)) % m;
+    }
+    return M[n];
+}
+int main()
+{
+    M[0] = 0; M[1] = 1;
+    cin >> a;
+    cout << (f(a) * f(a + 1)) % m;
+}
