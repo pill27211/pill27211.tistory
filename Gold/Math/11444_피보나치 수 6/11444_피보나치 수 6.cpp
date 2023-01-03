c5a8125a3249d51e7727f4b1a1538c49986b46b6
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-01-03 23:45:00 +0900

    Create 11444_피보나치 수 6.cpp

---
+++ Gold/Math/11444_피보나치 수 6/11444_피보나치 수 6.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+map <ll, ll> M;
+ll n, m = (ll)1e9 + 7;
+
+ll f(ll n)
+{
+    if(!M.count(n))
+    {
+        ll p = f(n / 2 + (n & 1 ? 1 : -1)), q = f(n >> 1);
+        M[n] = (n & 1 ? (p * p + q * q) : ((2 * p + q) * q)) % m;
+    }
+    return M[n];
+}
+int main()
+{
+    M[0] = 0; M[1] = 1;
+    cin >> n;
+    cout << f(n);
+}
