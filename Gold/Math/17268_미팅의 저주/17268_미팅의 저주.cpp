4ef79b4853d9d60e751244f1cbaaaefd2c911999
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-01-05 21:57:53 +0900

    Update 17268_미팅의 저주.cpp

--- Gold/Math/17268_미팅의 저주/17268_미팅의 저주.cpp
+++ Gold/Math/17268_미팅의 저주/17268_미팅의 저주.cpp
@@ -5,7 +5,7 @@ long long n, i, dp[10001]{1, 1, 2};
 int main()
 {
     for (n = 3; n < 10001; n++)
-		for (i = 0; i < n; dp[n] %= 987654321, i++)
-		  dp[n] += dp[i] * dp[n - i - 1];
+	for (i = 0; i < n; dp[n] %= 987654321, i++)
+		dp[n] += dp[i] * dp[n - i - 1];
     cin >> n, cout << dp[n >> 1] << '\n';
 }
