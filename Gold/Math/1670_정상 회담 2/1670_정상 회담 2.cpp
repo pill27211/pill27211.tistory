6366b20ea192d9dc0d47ee39a323657fdf11abd5
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-01-05 22:17:29 +0900

    Create 1670_정상 회담 2.cpp

---
+++ Gold/Math/1670_정상 회담 2/1670_정상 회담 2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long n, i, dp[10001]{1, 1, 2};
+int main()
+{
+    for (n = 3; n < 10001; n++)
+		for (i = 0; i < n; dp[n] %= 987654321, i++)
+			dp[n] += dp[i] * dp[n - i - 1];
+    cin >> n, cout << dp[n >> 1] << '\n';
+}
