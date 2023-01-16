4c9cff76077a5a17ea71454c975c3dfe78d968ce
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-01-16 19:35:41 +0900

    Update 27212_미팅.cpp

--- Gold/DP/27212_미팅/27212_미팅.cpp
+++ Gold/DP/27212_미팅/27212_미팅.cpp
@@ -10,14 +10,14 @@ ll f(int p, int q)
 {
 	if (p == n + 1 || q == m + 1)
 		return 0;
-    ll& t(dp[p][q]);
+        ll& t(dp[p][q]);
 	if (t == -1)
-        t = 0, t = max({C[a[p]][b[q]] + f(p + 1, q + 1), f(p, q + 1), f(p + 1, q)});
+            t = 0, t = max({C[a[p]][b[q]] + f(p + 1, q + 1), f(p, q + 1), f(p + 1, q)});
 	return t;
 }
 int main()
 {
-    ios_base::sync_with_stdio(0); cin.tie(0);
+        ios_base::sync_with_stdio(0); cin.tie(0);
 	memset(dp, -1, sizeof dp);
 	cin >> n >> m >> c;
 	for (i = 1; i <= c; i++)
