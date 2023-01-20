5afe940eb674459d18df80d498c643b2b2b23a71
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-01-20 15:27:58 +0900

    Create 1197_최소 스패닝 트리.cpp

---
+++ Gold/Graphs/1197_최소 스패닝 트리/1197_최소 스패닝 트리.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct E
+{
+	int p, q, w;
+	bool operator<(E& e) { return w < e.w; }
+}Eg[100001];
+int P[10001];
+int v, e, i, r;
+
+int f(int p) { return P[p] = P[p] == p ? p : f(P[p]); }
+void in()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	iota(P, P + 10001, 0);
+	cin >> v >> e;
+	for (; i < e; i++)
+		cin >> Eg[i].p >> Eg[i].q >> Eg[i].w;
+}
+void sv()
+{
+	sort(Eg, Eg + e);
+	for (i = 0; i < e; i++)
+	{
+		int p(f(Eg[i].p)), q(f(Eg[i].q));
+		if (p ^ q)
+			r += Eg[i].w, (p > q ? P[p] = q : P[q] = p);
+	}
+	cout << r;
+}
+int main()
+{
+	in();
+	sv();
+}
