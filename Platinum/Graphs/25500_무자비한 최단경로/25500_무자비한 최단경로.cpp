e93d6cab0e5b83a90937c0ab41b22aecb2b6eae3
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-01-21 16:25:59 +0900

    Update 25500_무자비한 최단경로.cpp

--- Platinum/Graphs/25500_무자비한 최단경로/25500_무자비한 최단경로.cpp
+++ Platinum/Graphs/25500_무자비한 최단경로/25500_무자비한 최단경로.cpp
@@ -1,77 +1,66 @@
 #include<bits/stdc++.h>
-#define ll long long
-#define P pair<ll, ll>
+#define pb push_back
+#define N 400001
 using namespace std;
 
-vector <P> Vx;
-vector <P> Vy;
-vector <P> V[400001];
-ll D[400001];
-ll N, K, i, j;
-ll x, y, z, p, q;
+struct S
+{
+	int p, q;
+	bool operator<(S& s) {return p ^ s.p ? p < s.p : q < s.q;}
+};
+vector <S> Vx, Vy, V[N];
+int D[N];
+int n, k, i, x, y, z;
 
 void f1()
 {
-	ios_base::sync_with_stdio(false); cin.tie(nullptr);
-	fill(D, D + 400001, 1e18);
-	cin >> N >> K;
-
-	for (i = 1; i <= N; i++)
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	fill(D, D + N, 2e9);
+	cin >> n >> k;
+	for (i = 1; i <= n; i++)
 	{
 		cin >> x >> y >> z;
+		Vx.pb({ x, i });
+		Vy.pb({ y, i });
 
-		Vx.push_back({ x, i });
-		Vy.push_back({ y, i });
-
-		p = z % K;
-		q = (K - p) % K;
-		V[i].push_back({ N + 1 + p, z });
-		V[N + 1 + q].push_back({ i, z });
+		int p = z % k;
+		V[i].pb({ n + 1 + p, z });
+		V[n + 1 + (k - p) % k].pb({ i, z });
 	}
 }
 void f2()
 {
-	priority_queue <P> Q;
-	Q.push({ 0, 1 });
-	D[1] = 0;
-
-	while (!Q.empty())
+	sort(Vx.begin(), Vx.end());
+	sort(Vy.begin(), Vy.end());
+	for (i = 1; i < n; i++)
 	{
-		i = -Q.top().first;
-		j = Q.top().second;
-		Q.pop();
-
-		if (D[j] < i)
-			continue;
-		for (int k = 0; k < (int)V[j].size(); k++)
-		{
-			ll d = i + V[j][k].second;
-			if (d < D[V[j][k].first])
-			{
-				D[V[j][k].first] = d;
-				Q.push({ -d, V[j][k].first });
-			}
-		}
+		V[Vx[i].q].pb({ Vx[i - 1].q, abs(Vx[i].p - Vx[i - 1].p) });
+		V[Vx[i - 1].q].pb({ Vx[i].q, abs(Vx[i].p - Vx[i - 1].p) });
+		V[Vy[i].q].pb({ Vy[i - 1].q, abs(Vy[i].p - Vy[i - 1].p) });
+		V[Vy[i - 1].q].pb({ Vy[i].q, abs(Vy[i].p - Vy[i - 1].p) });
 	}
 }
 void f3()
 {
-	sort(Vx.begin(), Vx.end());
-	sort(Vy.begin(), Vy.end());
-
-	for (i = 1; i < N; i++)
+	priority_queue <pair<int, int>> Q;
+	Q.push({ 0, 1 }); D[1] = 0;
+	while (Q.size())
 	{
-		V[Vx[i].second].push_back({ Vx[i - 1].second, abs(Vx[i].first - Vx[i - 1].first) });
-		V[Vx[i - 1].second].push_back({ Vx[i].second, abs(Vx[i].first - Vx[i - 1].first) });
-		V[Vy[i].second].push_back({ Vy[i - 1].second, abs(Vy[i].first - Vy[i - 1].first) });
-		V[Vy[i - 1].second].push_back({ Vy[i].second, abs(Vy[i].first - Vy[i - 1].first) });
+		auto [a, b] = Q.top();
+		a *= -1, Q.pop();
+		if (D[b] >= a)
+			for (auto& i : V[b])
+			{
+				int d = a + i.q;
+				if (d < D[i.p])
+					D[i.p] = d, Q.push({ -d, i.p });
+			}
 	}
-	f2();
 }
-int main(void)
+int main()
 {
 	f1();
+	f2();
 	f3();
-	for (i = 1; i <= N; i++)
-		cout << D[i] << '\n';
+	for (i = 1; i <= n; cout << D[i++] << '\n');
 }
